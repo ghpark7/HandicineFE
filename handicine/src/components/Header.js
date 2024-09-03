169e7120a0d2eec74a72ad769c1f1f990a61f605
Geonhwi Park <ghpark715@gmail.com>
2024-09-03 13:40:21 +0900

    feat: header에 로그인, 회원가입 버튼추가

--- handicine/src/components/Header.js
+++ handicine/src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 function Header() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" expand="lg" style={{ padding: '20px 10px' }}>
       <Container>
         <Navbar.Brand href="#home">Handicine</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Button variant="light" className="me-2">로그인</Button>
+            <Button variant="light">회원가입</Button>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
